481e132752d74f19ee1870e78b0d00619b450a64
ChandradipMoonlight <chandradipshivankar@gmail.com>
2022-10-17 22:20:36 +0530

    Creating: Service Registry

---
+++ service-registry/src/main/java/com/dailycode/serviceregistory/ServiceRegistoryApplication.java
@@ -0,0 +1,13 @@
+package com.dailycode.serviceregistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistoryApplication.class, args);
+	}
+
+}
