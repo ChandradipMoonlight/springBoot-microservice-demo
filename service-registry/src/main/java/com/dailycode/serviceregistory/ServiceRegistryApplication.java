eb730a0ea3024ca3205f3e8d4a94bef5997ebb7b
ChandradipMoonlight <chandradipshivankar@gmail.com>
2022-10-17 22:32:52 +0530

    implemented: Service Registry

--- service-registry/src/main/java/com/dailycode/serviceregistory/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/dailycode/serviceregistory/ServiceRegistryApplication.java
@@ -2,12 +2,14 @@ package com.dailycode.serviceregistory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class ServiceRegistoryApplication {
+@EnableEurekaServer
+public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistoryApplication.class, args);
+		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
 }
